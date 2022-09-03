4b7f82c7650c5aeca2e1190c66a60571ed81c951
jlmansilla <juanmancillaster@gmail.com>
2022-09-02 21:15:10 -0400

    cambio de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :countries
+  resources :users, only: %i[ create show update delete index]
+  resources :countries, only: %i[ index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
